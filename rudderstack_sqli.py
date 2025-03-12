9ba690d16d7e5ff3df409c0141b09f1bb54c156e
AleksaZatezalo <zabumaphu@gmail.com>
2025-03-12 15:59:43 -0400

    Exploit template updated and simplified

--- rudderstack_sqli.py
+++ rudderstack_sqli.py
@@ -1,26 +1,31 @@
 """
 Author: Aleksa Zaztezalo
 Date: May 2025
-Description: SQLi to RCE in Rudderstack.
+Description: SQLi to RCE in Rudderstack. It get's the flag from /flag.txt.
 """
 
-def gen_revshell(ip, port):
-    pass
-
 def gen_sqli(ip, port):
+    """
+    Takes two strings, ip and port, and generates an sqli payload. The SQLi
+    port sends a request to containing tht tontents of /flag.txt to http://ip:port.
+    """
     pass
 
-def start_server(ip, port):
+def send_request(url, proxy, ip, port):
+    """
+    Takes a targert url, url, a proxy url, and the ip and port of a
+    reverse shell listener. Sends a SQLi request.
+    """
     pass
 
-def send_request(url, proxy):
+def start_server(port):
+    """
+    Opens an http simple server on the specified port.
+    """
     pass
-
-def open_listener(port):
-    pass
-
-def interactive_shell(url, port):
-    pass
-
+    
 def main():
+    pass
+
+if __name__ == "__main__":
     pass
